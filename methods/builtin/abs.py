351d296ff0cad6bbbd093662e2f63609d1c36bea
lokeshgupta1981 <howtodoinjava@gmail.com>
2024-01-03 04:27:20 +0530

    Python Methods

---
+++ methods/builtin/abs.py
@@ -0,0 +1,14 @@
+# For a positive integer
+print(abs(10))  # Output: 10
+
+# For a negative integer
+print(abs(-10))  # Output: 10
+
+# For a positive floating-point number
+print(abs(10.5))  # Output: 10.5
+
+# For a negative floating-point number
+print(abs(-10.5))  # Output: 10.5
+
+# For a complex number
+print(abs(3 + 4j))  # Output: 5.0
